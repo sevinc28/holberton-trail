5735f2e2efcc939a8f5bf70d5badab58d770c81a
sevinc28 <jhasimova28@gmail.com>
2025-07-22 16:23:55 +0400

    task1

---
+++ list.py
@@ -0,0 +1,12 @@
+my_list = []
+my_list.append("alma")
+my_list.append("armud")
+my_list.append("banan")
+my_list.append("nar")
+my_list.append("erik")
+my_list.append("kivi")
+for item in my_list:
+    print(item)
+print(my_list[2])
+last = my_list.pop()
+print(last)
