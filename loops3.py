cdeb6aef71bbabfd8b5a19eb7ccd853a6b83237a
sevinc28 <jhasimova28@gmail.com>
2025-07-22 16:10:21 +0400

    task3

---
+++ loops3.py
@@ -0,0 +1,8 @@
+birinci_eded = 28
+ikinci_eded = 17
+if birinci_eded > ikinci_eded:
+    print(f"{birinci_eded} boyukdur {ikinci_eded}")
+elif birinci_eded < ikinci_eded:
+    print(f"{birinci_eded} kicikdir {ikinci_eded}")
+else:
+    print(f"{birinci_eded} beraberdir {ikinci_eded}")
