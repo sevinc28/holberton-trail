6459c24292f63e15329ee933750335d40ca5c903
sevinc28 <jhasimova28@gmail.com>
2025-07-22 15:58:20 +0400

    task1

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+#Ad, soyad ve yas olaraq 3 deyisken teyin edirik
+ad = "Sevinc"
+soyad = "Hasimova"
+yas = 19
+#string interpolationdan istifade edib cap edirik
+print(f"Salam, menim adim {ad}, menim soy adim {soyad}. {yas} yasim var.")
